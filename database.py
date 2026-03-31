78d4eb6665f7594f8b2662366aafb8717b7c8b59
Aditya Gouda <AAGA2150@Xitadelint.com>
2026-03-31 16:53:02 +0530

    added superbase database

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./saas.db"
+# DATABASE_URL = "sqlite:///./saas.db"
+DATABASE_URL = "postgresql://postgres.bwytctyavlypxbueqykr:aaga!2150b552919101@aws-1-ap-south-1.pooler.supabase.com:5432/postgres"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autoflush=False)
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
